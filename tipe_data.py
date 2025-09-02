ee0f9e0db60b089ffbd437cf4aaaccad585abd96
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-02 22:53:52 +0700

    tipe data

---
+++ tipe_data.py
@@ -0,0 +1,18 @@
+# di python ada berbagai macam tipe data
+
+# string -> sebuah teks
+nama = "Nugie kurniawan"
+
+# integer(int) -> bilangan bulat tanpa koma
+umur = 21
+
+# float -> bilangan desimal alias dengan koma
+uang = 21.000
+
+# Boolean -> tipe data binary yang hanya mengandung 2 nilai True or False
+is_have_girlfriend = False
+
+print(nama)
+print(umur)
+print(uang)
+print("apakah ",nama," sudah mempunyai pacar ? ",str(is_have_girlfriend))
