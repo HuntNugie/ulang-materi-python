bcb8b2f4d37b81ea25d2e1e65b274c251181029f
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-02 22:57:08 +0700

    tipe data

---
+++ input.py
@@ -0,0 +1,8 @@
+# kita bisa menggunakan inputan dari user
+nama = input("Masukan nama anda : ") # fungsi input akan selalu menghasilkan tipe data string apapun nilai nya
+print("Hello ",nama)
+
+umur = int(input("Masukan umur anda : ")) # jika ingin mendapatkan tipe data yang lain harus di konversi terlebih dahulu
+print("Umur anda : ",umur)
+print("tipe data umur : ",type(umur)) # function type berfungsi untuk mendapatkan tipe data nya itu apa
+
