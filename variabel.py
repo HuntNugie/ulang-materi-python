18a44d66da7d883c8290cdf6c267e8b417625a17
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-02 22:48:44 +0700

    variabel

---
+++ variabel.py
@@ -0,0 +1,24 @@
+# variabel merupakan wadah untuk nilai data 
+# penamaan sebuah variabel tidak boleh(di awali angka,menggunakan spasi,menggunakan tanda minus)
+
+nama = "Nugie kurniawan"
+# 10nama tidak boleh
+
+# memasukan data ke variabel
+
+# assigment biasa
+umur = 21
+
+# multiline assigment
+a = b = c = 10 # ini artinya variabel a,b,c itu bernilai 10
+mahasiswa, nim = "Nugie kurniawan",24010004 #ini artinya akan memasukan nilai sesuai posisi sebelum koma
+
+
+
+# mencetak variabel
+print(nama)
+print(a)
+print(b)
+print(c)
+print("nama mahasiswa = ",mahasiswa)
+print("nim mahasiswa = ",nim)
