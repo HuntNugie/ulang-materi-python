a8fb1a2ff25c466af013ec0fd16325f58019285d
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-03 20:11:57 +0700

    aritmatika

---
+++ aritmatika.py
@@ -0,0 +1,45 @@
+# aritmatika merupakan operator yang bertugas mengoperasikan oeprasi matematika
+
+# tambah
+a = 5
+b = 4
+hasil = a+b
+print(hasil)
+
+# kurang
+a = 10
+b = 2
+hasil = a-b
+print(hasil)
+
+# kali
+a = 10000
+b = 2
+hasil = a*b
+print(f"{hasil:,}")
+
+# bagi
+a = 10000
+b = 2
+hasil = a/b
+print(f"{hasil:,}")
+
+# pembagian bulat (floor division)
+a = 10000
+b = 2
+hasil = a//b
+print(f"{hasil:,}")
+
+# sisa bagi(modulus)
+a = 10
+b = 3
+hasil = a%b
+print(hasil)
+
+# pangkat (exponen)
+a = 2
+b = 4
+hasil = a**b
+print(hasil)
+
+
