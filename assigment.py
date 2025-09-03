1918d6a728a014f10d5a7b6ce672fd741cd1f64e
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-03 20:53:21 +0700

    assigment

---
+++ assigment.py
@@ -0,0 +1,35 @@
+# operator assigment(penugasan) operator yang berfungsi memanipulasi nilai sebelah kanan ke variabel sebelah kiri
+
+# memasukan nilai ke dalam variabel(=)
+a = 10
+print(a)
+
+# menambahkan nilai sebelum nya dengan nilai baru
+a+=5
+print(a)
+
+# mengurangi nilai sebelumnya dengan nilai baru
+a-=5
+print(a)
+
+# mengkalikan nilai sebelumnya dengan nilai baru
+a*=2
+print(a)
+
+# membagi nilai sebelumnya dengan nilai baru
+a/=2
+print(a)
+
+# membagi bulat(floor division) nilai sebelumnya dengan nilai baru
+a = 10
+a//=2
+print(a)
+
+# sisa bagi(modulus) nilai sebelumnya dengan nilai baru
+a%=2
+print(a)
+
+# pangkat(exponen) nilai sebelumnya dengan pangkat nilai baru
+a = 10
+a**=4
+print(a)
