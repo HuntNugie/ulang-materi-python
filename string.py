c036fd1623a52d345315e9104fa55f1a93489300
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-05 22:39:39 +0700

    operator string

---
+++ string.py
@@ -0,0 +1,17 @@
+# operator string merupakan operator yang berhubungan dan memanipulasi string
+
+# operator concatention(+) -> berfungsi untuk menyambung string
+namaDepan = "Nugie"
+namaBelakang = "kurniawan"
+namaLengkap = namaDepan + " " + namaBelakang
+print(namaLengkap)
+
+# repetition (*) -> berfungsi untuk mengkalikan string 
+ulang = "ulang"
+print(ulang * 3)
+
+# membership (in) -> berfungsi untuk mengecek apakah suatu string ada di dalam string lain dan akna menghasilkan nilai boolean
+string = "saya sedang coding python"
+print("python" in string)
+print("javascript" in string)
+print("sedang" in string)
