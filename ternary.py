9d1c2c1003889549799c43da689626f1bb5b94e3
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-06 23:12:34 +0700

    kontrol alur ternary operator

---
+++ ternary.py
@@ -0,0 +1,8 @@
+# ternary operator singkatnya merupakan bentuk singkat dari if else jika kondisinya hanya memiliki 1 kondisi jika ga bener berarti masuk ke else
+
+# program bilangan genap dan ganjil
+inputan = int(input("Masukan angka : "))
+
+hasil = "Bilangan genap" if inputan % 2 == 0 else "Bilangan ganjil"
+
+print(hasil) 
