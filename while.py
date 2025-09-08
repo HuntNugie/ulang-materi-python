8d95edd6f78e3a894b8a048657033a2e43a88ef3
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-08 23:38:46 +0700

    looping while

---
+++ while.py
@@ -0,0 +1,12 @@
+# while merupakan perluangan yang menggunakan kondisi selama kondisinya true maka program akan selalu berjalna
+angka = 0
+# while angka <= 10 :
+#     print(angka)
+#     angka+=1
+    
+array = ["Nugie","laurin","nadin"]
+
+while angka < len(array):
+    print(array[angka])
+    angka +=1
+    
