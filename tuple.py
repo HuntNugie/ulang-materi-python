4faeed399cb64e13348bab582a3b98811f6d6d0d
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-12 00:37:08 +0700

    tuple python

---
+++ tuple.py
@@ -0,0 +1,8 @@
+# tuple mirip list di python hanya saja tidak bisa di ubah isinya (immutable) 
+
+tempat_tanggal_Lahir = ("Bandung",30,11,2004)
+print(tempat_tanggal_Lahir)
+
+# bisa di looping
+for i in tempat_tanggal_Lahir:
+    print(i)
